78d40d13f2f5017ffa8ce0356bcdeb6fe8c124b5
Leo Simon <leo.simvn@gmail.com>
2023-12-14 17:38:42 +0100

    front-end add

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors'
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :response-time ms :body'));
